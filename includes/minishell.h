a5c73f2dc27a456524d417cc337ee3dad8f81127
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-02-11 21:04:00 +0100

    feat: add structures for commands and pipes

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 18:54:06 by vmuller           #+#    #+#             */
-/*   Updated: 2023/02/07 18:40:12 by troberts         ###   ########.fr       */
+/*   Updated: 2023/02/11 21:02:38 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,51 @@
 # include "libft.h"
 
 # include "parsing.h"
+
+/**
+ * @brief Enum containing all of the different sorts of token possible.
+ * 
+ */
+typedef enum e_token_type
+{
+	cmd,
+	pipe
+}	t_token_type;
+
+/**
+ * @brief Structure used for commands token.
+ * 
+ * @param cmd_name CMD NAME, ex: ls. From parsing.
+ * @param cmd_path Cmd path, ex : /bin/ls
+ * @param cmd_args Cmd args, ex: "-l". From parsing, modified after pre-process.
+ * @param envp Environment pointer. From parsing.
+ * @param fd_in fd used for input. From pre-process.
+ * @param fd_out fd used for output. From pre-process.
+ */
+typedef struct s_cmd {
+	char	*cmd_name;
+	char	*cmd_path;
+	char	**cmd_args;
+	char	**envp;
+	int		fd_in;
+	int		fd_out;
+}			t_cmd;
+
+/**
+ * @brief Structure used for pipes token.
+ * 
+ * @param fd Initialised from pipe(). From pre-process.
+ */
+typedef struct s_pipe {
+	int	fd[2];
+}		t_pipe;
+
+typedef struct s_token {
+	t_token_type	token_type;
+	void			*content;
+	struct s_token	*next;
+	struct s_token	*previous;
+}					t_token;
 
 typedef struct s_mini	t_mini;
 

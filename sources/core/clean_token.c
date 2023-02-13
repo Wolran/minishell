614789311eb5d48da6def89b2854e6733b43f69c
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-02-13 20:14:30 +0100

    feat: add clean functions

---
+++ sources/core/clean_token.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clean_token.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 17:23:56 by troberts          #+#    #+#             */
+/*   Updated: 2023/02/13 20:06:15 by troberts         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	clean_node(t_token_exe *tokens)
+{
+	t_cmd	*cmd_t;
+	t_pipe	*pipe_t;
+
+	if (tokens->token_type == cmd)
+	{
+		cmd_t = tokens->content;
+		free(cmd_t->cmd_name);
+		ft_free_double_ptr(cmd_t->cmd_args);
+	}
+	else if (tokens->token_type == pipe_token)
+	{
+		pipe_t = tokens->content;
+		close(pipe_t->fd[PIPE_READ]);
+		close(pipe_t->fd[PIPE_WRITE]);
+	}
+}
+
+void	clean_tokens_struct(t_token_exe *tokens)
+{
+	t_token_exe	*previous_node;
+
+	while (tokens)
+	{
+		clean_node(tokens);
+		previous_node = tokens;
+		tokens = tokens->next;
+		free(previous_node->content);
+		free(previous_node);
+	}
+}

134805a982f2c6f86129075569b5dab326cd4648
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-02-16 02:40:49 +0100

    fix: leaks with env

--- sources/builtins/run_env.c
+++ sources/builtins/run_env.c
@@ -6,7 +6,7 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 02:08:55 by troberts          #+#    #+#             */
-/*   Updated: 2023/02/16 02:18:26 by troberts         ###   ########.fr       */
+/*   Updated: 2023/02/16 02:31:17 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,16 @@
 
 int	run_env(t_minishell *minishell, t_cmd *cmd)
 {
+	char	**envp;
+
 	(void)cmd;
-	print_double_char(convert_env_to_char(minishell->envp));
+	envp = convert_env_to_char(minishell->envp);
+	if (envp == NULL)
+	{
+		printf("echo: malloc error\n");
+		return (EXIT_FAILURE);
+	}
+	print_double_char(envp);
+	free(envp);
 	return (EXIT_SUCCESS);
 }

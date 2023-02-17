e7ff87c6531032866dcd3313c300263c1667ef4f
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-02-17 02:52:39 +0100

    feat: add pwd builtin

--- sources/builtins/run_pwd.c
+++ sources/builtins/run_pwd.c
@@ -6,7 +6,7 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 02:08:55 by troberts          #+#    #+#             */
-/*   Updated: 2023/02/16 02:18:26 by troberts         ###   ########.fr       */
+/*   Updated: 2023/02/17 02:47:28 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,21 @@
 
 int	run_pwd(t_minishell *minishell, t_cmd *cmd)
 {
+	char	*path;
+
 	(void)minishell;
-	(void)cmd;
-	printf("Not working.\n");
-	return (EXIT_SUCCESS);
+	if (size_double_char(cmd->cmd_args) != 1)
+		return (this_is_ub(EXIT_FAILURE));
+	path = getcwd(NULL, 0);
+	if (path == NULL)
+	{
+		perror("bash: pwd");
+		return (EXIT_FAILURE);
+	}
+	else
+	{
+		ft_putendl_fd(path, STDOUT_FILENO);
+		free(path);
+		return (EXIT_SUCCESS);
+	}
 }

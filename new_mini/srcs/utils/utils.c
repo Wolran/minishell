39ff97baa327c08b406ac198ded8783b0873c7fd
Wolran <38337827+Wolran@users.noreply.github.com>
2023-03-10 16:41:11 +0100

    annuler upload

--- new_mini/srcs/utils/utils.c
+++ new_mini/srcs/utils/utils.c
@@ -6,7 +6,7 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 23:33:56 by troberts          #+#    #+#             */
-/*   Updated: 2023/03/06 23:35:02 by troberts         ###   ########.fr       */
+/*   Updated: 2023/03/10 01:49:04 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,25 @@ int	perror_return(char *str, int return_code)
 {
 	perror(str);
 	return (return_code);
+}
+
+void	clean_minishell(t_mini *mini)
+{
+	ft_close(mini->in);
+	ft_close(mini->out);
+	close_fd(mini);
+	close(STDIN);
+	close(STDOUT);
+	free_token(mini->token);
+	free_env(mini->env);
+	free_all_export(mini->export);
+}
+
+void	clean_child(char *path, char **args, t_mini *mini, t_env *env)
+{
+	(void)env;
+	(void)args;
+	clean_minishell(mini);
+	ft_memdel(path);
+	free(args);
 }

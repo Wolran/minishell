a5ebca67e23e2b663658378279d869d68ea3f1fa
Thomas Robertson <5249646+ThomasRobertson@users.noreply.github.com>
2023-03-11 18:16:29 +0100

    fix: working here_doc

--- new_mini/srcs/parsing/here_doc.c
+++ new_mini/srcs/parsing/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: troberts <troberts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 02:31:30 by troberts          #+#    #+#             */
-/*   Updated: 2023/03/11 15:49:46 by troberts         ###   ########.fr       */
+/*   Updated: 2023/03/11 18:13:41 by troberts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ char	*get_input_stdin(char *limiter)
 {
 	char	*line;
 	char	*input_stdin;
+	char	*input_stdin_tmp;
 	size_t	len_limiter;
 
 	len_limiter = ft_strlen(limiter);
 	input_stdin = NULL;
 	while (1)
 	{
-		ft_printf("> ");
-		line = readline(NULL);
+		line = readline("> ");
 		if (line == NULL)
 		{
 			ft_printf("bash: warning: here-document delimited by end-of-file");
@@ -35,31 +35,50 @@ char	*get_input_stdin(char *limiter)
 			free(line);
 			return (input_stdin);
 		}
-		else
-			input_stdin = ft_strjoin_free(input_stdin, line);
+		input_stdin_tmp = ft_strjoin_free(input_stdin, line);
+		input_stdin = ft_strjoin(input_stdin_tmp, "\n");
+		free(input_stdin_tmp);
 	}
+}
+
+void	reopen_here_doc(t_mini *mini)
+{
+	mini->fdin = open(".heredoc", O_RDONLY);
+	if (mini->fdin == -1)
+	{
+		ft_putstr_fd("minishell: (here_doc_tmp_file) : ", STDERR);
+		ft_putendl_fd(strerror(errno), STDERR);
+		mini->ret = 1;
+		mini->no_run = 1;
+		return ;
+	}
+	unlink(".heredoc");
+	dup2(mini->fdin, STDIN);
 }
 
 void	here_doc(t_mini *mini, t_token *token)
 {
 	char	*input_heredoc;
+	int		fd;
 
-	ft_close(mini->fdin);
-	mini->fdin = open(".heredoc", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (mini->fdin == -1 || token->str == NULL || ft_strlen(token->str) == 0)
+	unlink(".heredoc");
+	fd = open(".heredoc", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fd == -1 || token->str == NULL || ft_strlen(token->str) == 0)
 	{
 		ft_putstr_fd("minishell: ", STDERR);
 		ft_putstr_fd(token->str, STDERR);
-		ft_putendl_fd(": No such file or directory", STDERR);
+		ft_putstr_fd(": ", STDERR);
+		ft_putendl_fd(strerror(errno), STDERR);
 		mini->ret = 1;
 		mini->no_run = 1;
 		return ;
 	}
 	input_heredoc = get_input_stdin(token->str);
 	if (input_heredoc == NULL)
-		ft_putstr_fd("", mini->fdin);
+		ft_putstr_fd("", fd);
 	else
-		ft_putstr_fd(input_heredoc, mini->fdin);
+		ft_putstr_fd(input_heredoc, fd);
 	free(input_heredoc);
-	dup2(mini->fdin, STDIN);
+	close(fd);
+	reopen_here_doc(mini);
 }
